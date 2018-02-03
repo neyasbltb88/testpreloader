b0121ae6c2f9adb5ef4545125d9786501bc02b44
Neyasbltb <neyasbltb_88@mail.ru>
2018-02-03 22:55:47 +0300

    Попытка адаптива №3 начало js

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,10 @@
-$(function() {
+function log(argument) {
+  console.log(argument);
+}
 
-	// Custom JS
-
-});
+(function resize() {
+  log(window);
+  var w_w = window.innerWidth;
+  var w_h = window.innerHeight;
+  log(w_w+'x'+w_h);
+})()
