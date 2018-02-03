7e99127ef995ae81411a9e6709c3514332ac86ab
Neyasbltb <neyasbltb_88@mail.ru>
2018-02-03 23:33:53 +0300

    Установка высоты на .loader

--- app/js/common.js
+++ app/js/common.js
@@ -1,10 +1,15 @@
 function log(argument) {
-  console.log(argument);
+    console.log(argument);
 }
 
 (function resize() {
-  log(window);
-  var w_w = window.innerWidth;
-  var w_h = window.innerHeight;
-  log(w_w+'x'+w_h);
-})()
+    log(window);
+    var w_w = window.innerWidth;
+    var w_h = window.innerHeight;
+    log(w_w + 'x' + w_h);
+    var loader = document.querySelector('.loader');
+    var loader_comp_style = getComputedStyle(loader);
+    var loader_w = parseFloat(loader_comp_style.width);
+    log(loader_w);
+    loader.style.height = loader_w + 'px';
+})();
