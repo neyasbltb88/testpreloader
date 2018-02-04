9852806d7f204e703ca0229732f4ac69847fd65d
Neyasbltb <neyasbltb_88@mail.ru>
2018-02-04 04:06:34 +0300

    Оказалось что не финальный)

--- app/js/common.js
+++ app/js/common.js
@@ -9,14 +9,16 @@ function log(argument) {
     // log(w_w + 'x' + w_h);
     var loader = document.querySelector('.loader');
     var loader_comp_style = getComputedStyle(loader);
-    var loader_w = parseFloat(loader_comp_style.width);
+    var loader_w = parseInt(loader_comp_style.width);
     var loader_ratio = w_w / loader_w;
 
     var segment = document.querySelectorAll('.segment');
     var segment_comp_style = getComputedStyle(segment[0]);
-    var segment_w = parseFloat(segment_comp_style.width);
+    var segment_w = parseInt(segment_comp_style.width);
     var segment_ratio = loader_w / segment_w;
 
+    log('loader_w: ' + parseInt(w_w / loader_ratio));
+    log('segment_w: ' + parseInt(segment_w));
 
     function preloader_resize() {
         var w_w = window.innerWidth;
@@ -24,10 +26,10 @@ function log(argument) {
         loader.style.height = w_w / loader_ratio + 'px';
 
         var segment_w = (w_w / loader_ratio) / segment_ratio;
-        // log('loader_w: ' + (w_w / loader_ratio));
-        // log('segment_w: ' + segment_w);
+        // log('loader_w: ' + parseInt(w_w / loader_ratio));
+        // log('segment_w: ' + parseInt(segment_w));
         for (var i = 0; i < segment.length; i++) {
-            segment[i].style.maxWidth = segment_w + 'px';
+            segment[i].style.maxWidth = parseInt(segment_w) + 'px';
         }
     }
 
