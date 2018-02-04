94249bfd932e70a3a1f61b31b61d280c577a7e5f
Neyasbltb <neyasbltb_88@mail.ru>
2018-02-04 03:59:23 +0300

    Возможно, финальный вариант адаптива

--- app/js/common.js
+++ app/js/common.js
@@ -2,54 +2,56 @@ function log(argument) {
     console.log(argument);
 }
 
-function preloader_resize() {
+(function init_preloader() {
     // log(window);
-    // var w_w = window.innerWidth;
-    // var w_h = window.innerHeight;
+    var w_w = window.innerWidth;
+    var w_h = window.innerHeight;
     // log(w_w + 'x' + w_h);
     var loader = document.querySelector('.loader');
     var loader_comp_style = getComputedStyle(loader);
     var loader_w = parseFloat(loader_comp_style.width);
-    // log(loader_w);
-    loader.style.height = loader_w + 'px';
+    var loader_ratio = w_w / loader_w;
 
     var segment = document.querySelectorAll('.segment');
-    // log(segment);
-
-    for (var i = 0; i < segment.length; i++) {
-        segment[i].style.maxWidth = 'initial';
-        // log(segment[i].style.maxWidth);
-    }
-
     var segment_comp_style = getComputedStyle(segment[0]);
-    // log(segment_comp_style);
     var segment_w = parseFloat(segment_comp_style.width);
-    // log(segment_w);
-    for (var i = 0; i < segment.length; i++) {
-        segment[i].style.maxWidth = segment_w + 'px';
-        // log(segment[i].style.maxWidth);
+    var segment_ratio = loader_w / segment_w;
+
+
+    function preloader_resize() {
+        var w_w = window.innerWidth;
+
+        loader.style.height = w_w / loader_ratio + 'px';
+
+        var segment_w = (w_w / loader_ratio) / segment_ratio;
+        // log('loader_w: ' + (w_w / loader_ratio));
+        // log('segment_w: ' + segment_w);
+        for (var i = 0; i < segment.length; i++) {
+            segment[i].style.maxWidth = segment_w + 'px';
+        }
     }
-}
 
-preloader_resize();
+    preloader_resize();
 
-(function() {
-    var throttle = function(type, name, obj) {
-        obj = obj || window;
-        var running = false;
-        var func = function() {
-            if (running) { return; }
-            running = true;
-            requestAnimationFrame(function() {
-                obj.dispatchEvent(new CustomEvent(name));
-                running = false;
-            });
+    (function() {
+        var throttle = function(type, name, obj) {
+            obj = obj || window;
+            var running = false;
+            var func = function() {
+                if (running) { return; }
+                running = true;
+                requestAnimationFrame(function() {
+                    obj.dispatchEvent(new CustomEvent(name));
+                    running = false;
+                });
+            };
+            obj.addEventListener(type, func);
         };
-        obj.addEventListener(type, func);
-    };
 
-    /* init - you can init any event */
-    throttle("resize", "optimizedResize");
+        /* init - you can init any event */
+        throttle("resize", "optimizedResize");
+    })();
+
+    window.addEventListener("optimizedResize", preloader_resize);
+
 })();
-
-window.addEventListener("optimizedResize", preloader_resize);
