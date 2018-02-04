ff90cf374389e0c0e89363275551c9eb9fb3840b
Neyasbltb <neyasbltb_88@mail.ru>
2018-02-04 23:21:45 +0300

    Добавил удаление прелоадера по клику

--- app/js/common.js
+++ app/js/common.js
@@ -1,40 +1,13 @@
-function log(argument) {
-    console.log(argument);
-}
+var w_w = window.innerWidth;
+var loader = document.querySelector('.loader');
+var loader_comp_style = getComputedStyle(loader);
+var loader_w = parseInt(loader_comp_style.width);
+var loader_ratio = w_w / loader_w;
+var body = document.querySelector('body');
+var loader_wrap = document.querySelector('#loader_wrap');
 
-(function init_preloader() {
-    // log(window);
-    var w_w = window.innerWidth;
-    var w_h = window.innerHeight;
-    // log(w_w + 'x' + w_h);
-    var loader = document.querySelector('.loader');
-    var loader_comp_style = getComputedStyle(loader);
-    var loader_w = parseInt(loader_comp_style.width);
-    var loader_ratio = w_w / loader_w;
-
-    var segment = document.querySelectorAll('.segment');
-    var segment_comp_style = getComputedStyle(segment[0]);
-    var segment_w = parseInt(segment_comp_style.width);
-    var segment_ratio = loader_w / segment_w;
-
-    log('loader_w: ' + parseInt(w_w / loader_ratio));
-    log('segment_w: ' + parseInt(segment_w));
-
-    function preloader_resize() {
-        var w_w = window.innerWidth;
-
-        loader.style.height = w_w / loader_ratio + 'px';
-
-        // var segment_w = (w_w / loader_ratio) / segment_ratio;
-        // // log('loader_w: ' + parseInt(w_w / loader_ratio));
-        // // log('segment_w: ' + parseInt(segment_w));
-        // for (var i = 0; i < segment.length; i++) {
-        //     segment[i].style.maxWidth = (segment_w) + 'px';
-        // }
-    }
-
+function init_preloader() {
     preloader_resize();
-
     (function() {
         var throttle = function(type, name, obj) {
             obj = obj || window;
@@ -49,11 +22,34 @@ function log(argument) {
             };
             obj.addEventListener(type, func);
         };
-
-        /* init - you can init any event */
         throttle("resize", "optimizedResize");
     })();
 
     window.addEventListener("optimizedResize", preloader_resize);
 
-})();
+    // setTimeout(function() {
+    //     remove_preloader();
+    // }, 5000);
+}
+
+function preloader_resize() {
+    console.log('preloader_resize');
+    w_w = window.innerWidth;
+    loader.style.height = w_w / loader_ratio + 'px';
+}
+
+function remove_preloader() {
+    console.log('remove_preloader');
+    window.removeEventListener("optimizedResize", preloader_resize);
+    if (loader_wrap) {
+        loader_wrap.classList.add("opacity0");
+        setTimeout(function() {
+            body.removeChild(loader_wrap);
+        }, 1000);
+
+    }
+}
+
+loader.addEventListener("click", remove_preloader); //Для теста
+
+init_preloader();
