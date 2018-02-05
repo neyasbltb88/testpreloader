7307485403e0e8875e3414921a463be54dc44a58
Neyasbltb <neyasbltb_88@mail.ru>
2018-02-05 03:26:35 +0300

    Сделал переключаемый прелоадер - init_preloader, remove_preloader

--- app/js/common.js
+++ app/js/common.js
@@ -1,12 +1,20 @@
+var fade_time_preloader = 700; //Время исчезновения прелоадера, мс
+
 var w_w = window.innerWidth;
+var body = document.querySelector('body');
+var loader_wrap = document.querySelector('#loader_wrap');
+var loader_base_display = getComputedStyle(loader_wrap).display;
 var loader = document.querySelector('.loader');
 var loader_comp_style = getComputedStyle(loader);
 var loader_w = parseInt(loader_comp_style.width);
 var loader_ratio = w_w / loader_w;
-var body = document.querySelector('body');
-var loader_wrap = document.querySelector('#loader_wrap');
 
 function init_preloader() {
+    loader_wrap.style.display = loader_base_display;
+    setTimeout(function() {
+        loader_wrap.style.opacity = '1';
+    }, 0);
+
     preloader_resize();
     (function() {
         var throttle = function(type, name, obj) {
@@ -42,14 +50,16 @@ function remove_preloader() {
     console.log('remove_preloader');
     window.removeEventListener("optimizedResize", preloader_resize);
     if (loader_wrap) {
-        loader_wrap.classList.add("opacity0");
+        loader_wrap.style.transition = 'opacity ' + fade_time_preloader / 1000 + 's';
+        loader_wrap.style.opacity = '0';
         setTimeout(function() {
-            body.removeChild(loader_wrap);
-        }, 1000);
+            loader_wrap.style.display = 'none';
+        }, fade_time_preloader);
 
     }
 }
 
 loader.addEventListener("click", remove_preloader); //Для теста
+document.querySelector('#content_wrap').addEventListener("click", init_preloader); //Для теста
 
 init_preloader();
