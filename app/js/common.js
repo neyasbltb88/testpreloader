d727b4d94931081c7d42d8d3a27432b232cd1617
Neyasbltb <neyasbltb_88@mail.ru>
2018-02-06 20:45:53 +0300

    Изменил метод добавления классов анимации и gulpfile

--- app/js/common.js
+++ app/js/common.js
@@ -1,51 +1,36 @@
+"use strict";
 var loader_demo = true;
 var loader_square = true;
 
 var loader_wrap = document.querySelector('#loader_wrap');
-var loader_wrap_original_display = getComputedStyle(loader_wrap).display;
 var loader = document.querySelector('.loader');
 var content_wrap = document.querySelector('#content_wrap');
 
 var loader_fade_time_preloader = 700; //Время исчезновения прелоадера, мс
 var loader_time_demo = 3500; //Период переключения демо, мс
 
-var animation_classes = {
-    rotation: "loader_animation_rotation",
-    scale_before: "loader_animation_scale_before",
-    scale_after: "loader_animation_scale_after"
-};
+// var animation_classes = {
+//     rotation: "loader_animation_rotation",
+//     scale_before: "loader_animation_scale_before",
+//     scale_after: "loader_animation_scale_after"
+// };
+var animation_classes = [
+    "loader_animation_rotation",
+    "loader_animation_scale_before",
+    "loader_animation_scale_after"
+];
 
 var post_loader_transition_class = "blur";
-
-if (loader_square) {
-    init_optimizedResize();
-
-    function preloader_resize() {
-        console.log('preloader_resize');
-        var w_w = window.innerWidth;
-        var loader_comp_style = getComputedStyle(loader);
-        var loader_w = parseInt(loader_comp_style.width);
-        var loader_ratio = w_w / loader_w;
-        loader.style.height = w_w / loader_ratio + 'px';
-    }
-    init_preloader();
-} else {
-    init_preloader();
-}
-
-if (loader_demo) { //Для теста
-    init_loader_demo()
-}
+// ===============
+var loader_wrap_original_display = getComputedStyle(loader_wrap).display;
+animation_classes.push("active");
 
 function init_preloader(callback1, callback2) {
     console.log('init_preloader()');
     loader_wrap.style.display = loader_wrap_original_display;
-    loader_wrap.classList.add(
-        animation_classes.rotation,
-        animation_classes.scale_before,
-        animation_classes.scale_after
-    );
-    loader_wrap.classList.add("active");
+    for (var i = 0; i < animation_classes.length; i++) {
+        loader_wrap.classList.add(animation_classes[i]);
+    }
     content_wrap.classList.add(post_loader_transition_class);
     setTimeout(function() {
         loader_wrap.style.opacity = '1';
@@ -69,7 +54,7 @@ function init_preloader(callback1, callback2) {
             callback2();
         }
     }, loader_fade_time_preloader);
-}
+};
 
 function remove_preloader(callback1, callback2) {
     console.log('remove_preloader()');
@@ -86,12 +71,9 @@ function remove_preloader(callback1, callback2) {
         content_wrap.classList.remove(post_loader_transition_class);
         setTimeout(function() {
             loader_wrap.style.display = 'none';
-            loader_wrap.classList.remove(
-                animation_classes.rotation,
-                animation_classes.scale_before,
-                animation_classes.scale_after
-            );
-            loader_wrap.classList.remove("active");
+            for (var i = 0; i < animation_classes.length; i++) {
+                loader_wrap.classList.remove(animation_classes[i]);
+            }
             content_wrap.style.transition = '';
 
             if (typeof(callback1) == 'function') {
@@ -101,14 +83,7 @@ function remove_preloader(callback1, callback2) {
             }
         }, loader_fade_time_preloader);
     }
-}
-
-content_wrap.addEventListener("click", init_preloader); //Для теста
-content_wrap.style.cursor = 'pointer'; //Для теста
-
-loader.addEventListener("click", remove_preloader); //Для теста
-
-loader.style.cursor = 'pointer'; //Для теста
+};
 
 function init_optimizedResize() {
     var throttle = function(type, name, obj) {
@@ -125,7 +100,7 @@ function init_optimizedResize() {
         obj.addEventListener(type, func);
     };
     throttle("resize", "optimizedResize");
-}
+};
 
 function init_loader_demo() {
     loader_demo = false;
@@ -137,12 +112,42 @@ function init_loader_demo() {
             init_preloader(test_callback_remove);
         }
     }, loader_time_demo);
-}
+};
 
 function test_callback_add() {
     content_wrap.classList.add("loader_hideen");
-}
+};
 
 function test_callback_remove() {
     content_wrap.classList.remove("loader_hideen");
+};
+
+function preloader_resize() {
+    console.log('preloader_resize');
+    var w_w = window.innerWidth;
+    var loader_comp_style = getComputedStyle(loader);
+    var loader_w = parseInt(loader_comp_style.width);
+    var loader_ratio = w_w / loader_w;
+    loader.style.height = w_w / loader_ratio + 'px';
 }
+
+// ===============================
+
+if (loader_square) {
+    init_optimizedResize();
+
+
+    init_preloader();
+} else {
+    init_preloader();
+};
+
+if (loader_demo) { //Для теста
+    init_loader_demo()
+};
+
+content_wrap.addEventListener("click", init_preloader); //Для теста
+content_wrap.style.cursor = 'pointer'; //Для теста
+
+loader.addEventListener("click", remove_preloader); //Для теста
+loader.style.cursor = 'pointer'; //Для теста;
