f4b20c412a9bf5aa979e2a812df2ebc12f136ce7
Neyasbltb <neyasbltb_88@mail.ru>
2018-02-07 19:31:19 +0300

    Оптимизация производительности + стилизация скролла для webkit

--- app/js/loader.js
+++ app/js/loader.js
@@ -1,47 +1,52 @@
+;
 "use strict";
-var loader_demo = true;
-var loader_square = true;
+var loader_demo = true; //Включить/выключить режим демонстрации
+var loader_square = true; //Включить/выключить режим адаптивности
 
+var loader_fade_time_preloader = 500; //Время исчезновения прелоадера, мс
+var loader_time_demo = 3500; //Период переключения демо, мс
+
+var animation_classes = [ //Классы, которые будут добавляться на блок #content_wrap
+    "loader_animation", //Это базовый класс анимации, на его наличие привязана анимация в стилях
+]; //В массив можно добавлять свои классы 
+
+//Класс для дополнительной анимации проявления контента. 
+//Если он не нужен, то следует оставить пустую строку ("")
+//или пустую переменную (var post_loader_transition_class;)
+//Комментирование этой строки приведет к ошибке
+var post_loader_transition_class = "blur";
+// ===============
 var loader_wrap = document.querySelector('#loader_wrap');
 var loader = document.querySelector('.loader');
 var content_wrap = document.querySelector('#content_wrap');
 
-var loader_fade_time_preloader = 700; //Время исчезновения прелоадера, мс
-var loader_time_demo = 3500; //Период переключения демо, мс
-
-var animation_classes = [
-    "loader_animation_rotation",
-    "loader_animation_scale_before",
-    "loader_animation_scale_after"
-];
-
-var post_loader_transition_class = "blur";
-// ===============
 var loader_wrap_original_display = getComputedStyle(loader_wrap).display;
-animation_classes.push("active");
+content_wrap.style.transition = 'all ' + loader_fade_time_preloader / 1000 + 's';
+loader_wrap.style.transition = 'opacity ' + loader_fade_time_preloader / 1000 + 's';
 
 function init_preloader(callback1, callback2) {
-    // console.log('init_preloader()');
+    // console.log('init_preloader');
     loader_wrap.style.display = loader_wrap_original_display;
-    for (var i = 0; i < animation_classes.length; i++) {
-        loader_wrap.classList.add(animation_classes[i]);
+    if (animation_classes.length > 0) {
+        for (var i = 0; i < animation_classes.length; i++) {
+            loader_wrap.classList.add(animation_classes[i]);
+        }
     }
-    content_wrap.classList.add(post_loader_transition_class);
+    if (post_loader_transition_class) {
+        content_wrap.classList.add(post_loader_transition_class);
+    }
     setTimeout(function() {
-        loader_wrap.style.opacity = '1';
-        content_wrap.style.transition = 'all ' + loader_fade_time_preloader / 1000 + 's';
+        document.body.classList.add('loader');
     }, 10);
 
     if (loader_square) {
-        window.addEventListener("optimizedResize", preloader_resize);
         preloader_resize();
+        window.addEventListener("optimizedResize", preloader_resize);
     }
 
     setTimeout(function() {
-        document.body.style.overflowY = 'hidden';
-        document.body.style.position = 'fixed';
-        document.body.style.width = '100vw';
-        document.body.style.height = '100vh';
+        document.body.classList.add('dont_scroll');
+        document.body.classList.remove('content');;
 
         if (typeof(callback1) == 'function') {
             callback1();
@@ -52,25 +57,26 @@ function init_preloader(callback1, callback2) {
 };
 
 function remove_preloader(callback1, callback2) {
-    // console.log('remove_preloader()');
+    // console.log('remove_preloader');
     if (loader_square) {
         window.removeEventListener("optimizedResize", preloader_resize);
     }
     if (loader_wrap && content_wrap) {
-        loader_wrap.style.transition = 'opacity ' + loader_fade_time_preloader / 1000 + 's';
-        loader_wrap.style.opacity = '0';
-        document.body.style.overflowY = 'unset';
-        document.body.style.width = '';
-        document.body.style.height = '';
-        document.body.style.position = '';
-        content_wrap.classList.remove(post_loader_transition_class);
+
+        document.body.classList.remove('loader');
+        document.body.classList.remove('dont_scroll');
+        document.body.classList.add('content');
+
+        if (post_loader_transition_class) {
+            content_wrap.classList.remove(post_loader_transition_class);
+        }
         setTimeout(function() {
             loader_wrap.style.display = 'none';
-            for (var i = 0; i < animation_classes.length; i++) {
-                loader_wrap.classList.remove(animation_classes[i]);
+            if (animation_classes.length > 0) {
+                for (var i = 0; i < animation_classes.length; i++) {
+                    loader_wrap.classList.remove(animation_classes[i]);
+                }
             }
-            content_wrap.style.transition = '';
-
             if (typeof(callback1) == 'function') {
                 callback1();
             } else if (typeof(callback2) == 'function') {
@@ -118,7 +124,7 @@ function test_callback_remove() {
 };
 
 function preloader_resize() {
-    // console.log('preloader_resize');
+    console.log('preloader_resize');
     var w_w = window.innerWidth;
     var loader_comp_style = getComputedStyle(loader);
     var loader_w = parseInt(loader_comp_style.width);
